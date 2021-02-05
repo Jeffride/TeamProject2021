3ef1fa5cfb56409043a656e95351da2a0d506674
Nathan <crowley2305@gmail.com>
2021-02-05 13:53:08 +0000

    updated Css for leadboard not yet complete

--- teamProject/leaderboard/views.py
+++ teamProject/leaderboard/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import User
+from datetime import date
 
 # Create your views here.
 
 def leaderboard(request):
     number_of_users = User.objects.count()
-    context = {"number_of_users":number_of_users,
+    top_three = User.objects.all()[0:3] 
+    rest_of_users = User.objects.all()[3::]
+    today = date.today().strftime("%d/%m/%Y")
+    context = {'number_of_users':number_of_users,
                 'User_list':User.objects.all(),
+                'top_three':top_three,
+                'rest_of_users':rest_of_users,
+                'today':today,
+                
             }
-    return render(request, 'leaderboard/leaderboard.html', context)
+    return render(request, 'leaderboard.html', context)
