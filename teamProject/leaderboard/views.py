c0f59463ff984345f18001dce0f2b5783d53ea71
Nathan <crowley2305@gmail.com>
2021-02-06 14:44:24 +0000

    created larger cards for the top three of leaderboard

--- teamProject/leaderboard/views.py
+++ teamProject/leaderboard/views.py
@@ -7,9 +7,12 @@ from datetime import date
 # Create your views here.
 
 def leaderboard(request):
+    top_three_users=User.objects.all()[:3]
     context = {'number_of_users':User.objects.count(),
-                'User_list':User.objects.all(),
+                'rest_of_users':User.objects.all()[3:],
+                '1rst':top_three_users[0],
+                '2nd':top_three_users[1],
+                '3rd':top_three_users[2],
                 'today':date.today().strftime("%d/%m/%Y"),
-                
             }
     return render(request, 'leaderboard.html', context)
