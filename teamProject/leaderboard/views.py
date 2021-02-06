554cf313ef48ca21c9fd73a4edaecb6062991383
Nathan <crowley2305@gmail.com>
2021-02-06 02:21:47 +0000

    editing leaderboard css

--- teamProject/leaderboard/views.py
+++ teamProject/leaderboard/views.py
@@ -7,15 +7,9 @@ from datetime import date
 # Create your views here.
 
 def leaderboard(request):
-    number_of_users = User.objects.count()
-    top_three = User.objects.all()[0:3] 
-    rest_of_users = User.objects.all()[3::]
-    today = date.today().strftime("%d/%m/%Y")
-    context = {'number_of_users':number_of_users,
+    context = {'number_of_users':User.objects.count(),
                 'User_list':User.objects.all(),
-                'top_three':top_three,
-                'rest_of_users':rest_of_users,
-                'today':today,
+                'today':date.today().strftime("%d/%m/%Y"),
                 
             }
     return render(request, 'leaderboard.html', context)
