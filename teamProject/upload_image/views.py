f510f4960e4dc9c1de48a5a573acaa43012758b3
walebot <walesjj@gmail.com>
2021-03-03 14:26:37 +0000

    removing all images from directory

--- teamProject/upload_image/views.py
+++ teamProject/upload_image/views.py
@@ -9,8 +9,8 @@ def hotel_image_view(request):
         form = HotelForm(request.POST, request.FILES) 
   
         if form.is_valid(): 
-            form.save() 
-            return redirect('image_upload') 
+            #form.save() 
+            return redirect('success') 
     else: 
         form = HotelForm() 
     return render(request, 'hotel_image_form.html', {'form' : form}) 
