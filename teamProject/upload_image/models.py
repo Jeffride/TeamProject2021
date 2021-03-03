492923121b2ae5f8050469e16b158e4d618e5979
walebot <walesjj@gmail.com>
2021-03-03 14:47:24 +0000

    changed medium to hard. Working

--- teamProject/upload_image/models.py
+++ teamProject/upload_image/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Hotel(models.Model): 
     name = models.CharField(max_length=50) 
     hotel_Main_Img = models.ImageField(upload_to='easy/') 
-    hotel_medium_img = models.ImageField(upload_to='medium/')
+    hotel_medium_img = models.ImageField(upload_to='hard/')
