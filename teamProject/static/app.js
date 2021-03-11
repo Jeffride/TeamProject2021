6fe7f42d7a89f80a77d1bf3b89394e08181192cc
walebot <walesjj@gmail.com>
2021-03-11 10:37:08 +0000

    animation goes up and down

--- teamProject/static/app.js
+++ teamProject/static/app.js
@@ -505,3 +505,11 @@ const retroquestions = [
         ]
     }
 ]
+
+var object = document.getElementById('geotag');
+object.onclick=function(){
+  var x = Math.floor(Math.random()*300);
+  var y = Math.floor(Math.random()*300);
+  object.style.top = x + 'px';
+  object.style.left = y + 'px';
+};
