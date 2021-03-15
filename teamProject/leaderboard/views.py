a73237e6294f13d5bdc2b06425979f3e3ad664b5
walebot <walesjj@gmail.com>
2021-03-15 09:45:08 +0000

    additional @login required

--- teamProject/leaderboard/views.py
+++ teamProject/leaderboard/views.py
@@ -8,20 +8,22 @@ from datetime import date
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-'''def leaderboard(request):
-    if(User.objects.count()>3):
-        leaderboard=User.objects.order_by("high_score").reverse()
-        top_three_users=leaderboard[:3]
-        context = {'number_of_users':User.objects.count(),
-                    '1rst':top_three_users[0],
-                    '2nd':top_three_users[1],
-                    '3rd':top_three_users[2],
-                    'rest_of_users':leaderboard[3:],
-                    'today':date.today().strftime("%d/%m/%Y"),
-                }
-    else:
-        context={'number_of_users':User.objects.count()}
-    return render(request, 'leaderboard.html', context)'''
+'''
+    @login_required
+    def leaderboard(request):
+        if(User.objects.count()>3):
+            leaderboard=User.objects.order_by("high_score").reverse()
+            top_three_users=leaderboard[:3]
+            context = {'number_of_users':User.objects.count(),
+                        '1rst':top_three_users[0],
+                        '2nd':top_three_users[1],
+                        '3rd':top_three_users[2],
+                        'rest_of_users':leaderboard[3:],
+                        'today':date.today().strftime("%d/%m/%Y"),
+                    }
+        else:
+            context={'number_of_users':User.objects.count()}
+        return render(request, 'leaderboard.html', context)'''
 
 
 @login_required
