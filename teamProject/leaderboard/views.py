ec85147a7973524d11ee480dfb2fe19d32811c18
NathanCrowley <crowley2305@gmail.com>
2021-03-21 16:35:42 +0000

    added comments to models.py and views.py

--- teamProject/leaderboard/views.py
+++ teamProject/leaderboard/views.py
@@ -7,36 +7,23 @@ from datetime import date
 
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-
 '''
-@login_required
-    def leaderboard(request):
-        if(User.objects.count()>3):
-            leaderboard=User.objects.order_by("high_score").reverse()
-            top_three_users=leaderboard[:3]
-            context = {'number_of_users':User.objects.count(),
-                        '1rst':top_three_users[0],
-                        '2nd':top_three_users[1],
-                        '3rd':top_three_users[2],
-                        'rest_of_users':leaderboard[3:],
-                        'today':date.today().strftime("%d/%m/%Y"),
-                    }
-        else:
-            context={'number_of_users':User.objects.count()}
-        return render(request, 'leaderboard.html', context)'''
-
-
+    - userScores = dictionary to store each database users name and score. This allows me to loop through later and display the name and score of each user.
+    - sorted_values = convert userScores from a dictionary of sorted scores. But this lacks the usernames attached to each score.
+    - sorted_dict = is a completely sorted dictionary sorted on highscore with each key being a username and value being thier score.
+    - top_three = dictionary containing the top three users based on the sorted_dict.
+    - rest = dictionary of all users outside the top three.
+    - first = dictionary containing key(username):value(high_score) for the top user.
+    - second, third = same as first but for the second and third placed user.
+'''
 @login_required
 def leaderboard(request):
     userScores = {}
     username = request.user.username
     for user1 in Profile.objects.all():
         userScores[user1.user_name]=user1.high_score       
-    indexs = []
-    for x in range(len(userScores)):
-        indexs.append(x+1)
     dict1 = userScores
-    sorted_values = sorted(dict1.values(),reverse=True) # Sort the values
+    sorted_values = sorted(dict1.values(),reverse=True) #Sort the values
     sorted_dict = {}
     for i in sorted_values:
         for k in dict1.keys():
@@ -50,13 +37,11 @@ def leaderboard(request):
     third={k: top_three_users[k] for k in list(top_three_users)[2:3]}
     context={
         "users": sorted_dict.items(),
-        #"top_three":top_three_users.items(),
         "1rst": first.items(),
         "2nd": second.items(),
         "3rd": third.items(),
         "rest_of_users":rest.items(),
         "len_rest": len(rest.items()),
         "number_of_users": Profile.objects.count(),
-        "indexs":indexs[3:],
     }
     return render(request, 'leaderboard.html', context)
